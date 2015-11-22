45d933c76e8d289894a8ead8f7f68eb91c529a43
Brad Kupka <Brad Kupka>
2015-11-22 15:07:47 -0600

    Formatting changes

---
+++ src/java/Username.java
@@ -0,0 +1,47 @@
+package java;
+
+////////////////////////////////////////////////
+//File:    Server.java
+//Name:    Taber Hust, Michael Munzing, Brad Kupka
+//Class:   CS 4389
+//Date:    11/22/2015
+//
+//Final Project
+////////////////////////////////////////////////
+
+public class Username
+{
+	private String name;
+	private boolean taken;
+	
+	public Username(String name)
+	{
+		this.name = name;
+		this.taken = false;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public boolean getTaken()
+	{
+		return this.taken;
+	}
+	
+	public void setTaken()
+	{
+		this.taken = true;
+	}
+	
+	public void setOpen()
+	{
+		this.taken = false;
+	}
+}
